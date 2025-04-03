751ec6e2574c6366f69737fa837ef934d154591d
SRINIVASAN <srini172003@gmail.com>
2025-04-03 15:34:53 +0530

    datatypes
    java data types

---
+++ dataTypes.java
@@ -0,0 +1,19 @@
+package day;
+
+public class dataTypes {
+		public static void main(String[] args) {
+			byte age = 55;
+			short kilometers = 2569;
+			int number = 5698;
+			float cost = 265.4f;
+			long address = 5465168l;
+			
+			
+			System.out.println("byte"+age);
+			System.out.println("short"+kilometers);
+			System.out.println("byte"+number);
+			System.out.println("byte"+cost);
+			System.out.println("long"+address);
+		}
+
+}
